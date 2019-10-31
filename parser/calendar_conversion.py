7d13b06bdbf71f38cde3c0a2222a1b13dbb899ed
dsjack <dsjack89@gmail.com>
2019-10-30 17:34:05 -0700

    Adds methods to MarkdownCalendar to complete a calendar in markdown

--- parser/calendar_conversion.py
+++ parser/calendar_conversion.py
@@ -5,6 +5,8 @@ from parser import CalendarDict
  - Get desired name of output file user
  - Initialize parser object with path
  - Loop through OrderedDict and fill markdown table template
+ - MAKE ALL COLUMNS LEFT ALIGNED
+ - ADD ANCHOR TAGS FOR EACH LINK
 """
 
 calendar_dict = CalendarDict("../monthly_view.csv").table_rows
@@ -13,21 +15,24 @@ calendar_dict = CalendarDict("../monthly_view.csv").table_rows
 class MarkdownCalendar:
     def __init__(self, list_of_rows):
         self.list_of_rows = list_of_rows
-        self.calendar = self.make_col_titles()
+        self.calendar = self.make_col_titles() + self.make_rows()
 
     def make_col_titles(self):
-        header_string = "<tr>\n"
+        col_title_string = "<table>\n  <tbody>\n  <tr>\n"
         for col_titles in self.list_of_rows[0].keys():
-            header_string += "  <th>{}</th>\n".format(col_titles)
-        header_string += "</tr>"
+            col_title_string += "      <th>{}</th>\n".format(col_titles)
+        col_title_string += "    </tr>\n"
 
-        return header_string
+        return col_title_string
 
-    def make_row(self, dict_row):
-        row_string = "<tr>\n"
-        for values in dict_row.values():
-            row_string += "  <td>{}</td>\n".format(values)
-        row_string += "</tr>"
+    def make_rows(self):
+        row_string = ""
+        for row in self.list_of_rows:
+            row_string += "    <tr>\n"
+            for values in row.values():
+                row_string += "      <td>{}</td>\n".format(values)
+            row_string += "    </tr>\n"
+        row_string += "  </tbody>\n</table>"
 
         return row_string
 
