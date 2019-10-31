7f50715689f7eb3e974861c0b2a9c153a1ca6fcf
dsjack <dsjack89@gmail.com>
2019-10-30 17:12:56 -0700

    Removes redundant methods from CalendarDict class, makes a MarkdownCalendar object

--- parser/calendar_conversion.py
+++ parser/calendar_conversion.py
@@ -7,15 +7,29 @@ from parser import CalendarDict
  - Loop through OrderedDict and fill markdown table template
 """
 
-calendar_dict = CalendarDict("../monthly_view.csv").table_dict_rows
+calendar_dict = CalendarDict("../monthly_view.csv").table_rows
 
 
-def make_col_titles(list_of_dicts):
-    header_string = "<tr>\n"
-    for headers in list_of_dicts[0].keys():
-        header_string += "  <th>{}</th>\n".format(headers)
-    header_string += "<tr>"
+class MarkdownCalendar:
+    def __init__(self, list_of_rows):
+        self.list_of_rows = list_of_rows
+        self.calendar = self.make_col_titles()
 
-    print(header_string)
+    def make_col_titles(self):
+        header_string = "<tr>\n"
+        for col_titles in self.list_of_rows[0].keys():
+            header_string += "  <th>{}</th>\n".format(col_titles)
+        header_string += "</tr>"
 
-make_col_titles(calendar_dict)
+        return header_string
+
+    def make_row(self, dict_row):
+        row_string = "<tr>\n"
+        for values in dict_row.values():
+            row_string += "  <td>{}</td>\n".format(values)
+        row_string += "</tr>"
+
+        return row_string
+
+
+print(MarkdownCalendar(calendar_dict).calendar)
--- parser/calendar_dict.py
+++ parser/calendar_dict.py
@@ -12,30 +12,8 @@ class CalendarDict:
     def __init__(self, csv_file):
         self.__csv_file = csv_file
         self.__count = 0
-        self.__table_dict_cols = self.fill_dict_by_col()
-        self.__table_dict_rows = self.fill_dict_by_row()
-
-    def fill_dict_by_col(self):
-        """
-        Read and sort a csv file into a dictionary
-        :returns: dictionary
-        """
-        table_dict = OrderedDict()
-        with open(self.__csv_file, newline="", encoding='utf-8-sig') as csv_file:
-            reader = csv.reader(csv_file)
-            for row in reader:
-                if self.__count < 1:
-                    for j in range(len(row)):
-                        table_dict[row[j].strip()] = list()
-                else:
-                    self.__count = 1
-
-                    for key in table_dict.keys():
-                        table_dict[key].append(row[self.__count - 1].strip())
-                        self.__count += 1
-
-                self.__count += 1
-        return table_dict
+        # self.__table_dict_cols = self.fill_dict_by_col()
+        self.__table_rows = self.fill_dict_by_row()
 
     def fill_dict_by_row(self):
         rows = list()
@@ -46,15 +24,6 @@ class CalendarDict:
         return rows
 
     @property
-    def table_dict_cols(self):
-        """:returns: dictionary of table columns"""
-        return self.__table_dict_cols
-
-    @property
-    def table_dict_rows(self):
+    def table_rows(self):
         """:returns: list of table rows"""
-        return self.__table_dict_rows
-
-    def __str__(self):
-        return ''.join('{}{}'.format(key, val) for key, val in
-                       self.table_dict_cols.items())
+        return self.__table_rows
