68a94aca23d78aae0b8a701e6f02c5df0d6c5fd9
dsjack <dsjack89@gmail.com>
2020-02-11 23:09:06 -0800

    adds a unit test for the csv headers

--- test/basic_tests.py
+++ test/basic_tests.py
@@ -1,18 +1,26 @@
 import unittest
 from collections import OrderedDict
-
 from cal.calendar_dict import CalendarDict
 
 
 class InputTests(unittest.TestCase):
+
+    def setUp(self):
+        self.data = '../test_infile.csv'
+        self.table_rows = CalendarDict("../test_infile.csv").table_rows
+
+    def test_cal_headers(self):
+        self.assertEqual(list(self.table_rows[0].keys()),
+                         ['Headline', 'Status', 'Section', 'Author',
+                          'Draft due', 'Publish date', 'Link', 'Social posts'])
 
     def test_calendar_dict(self):
         """
         Tests whether a CalendarDict Instance's table_rows variable is a list
         of dictionaries
         """
-        table_rows = CalendarDict("../test_infile.csv").table_rows
-        self.assertIsInstance(table_rows, list)
-        for row in range(len(table_rows) - 1):
-            self.assertIsInstance(table_rows[row], OrderedDict)
+        print()
+        self.assertIsInstance(self.table_rows, list)
+        for row in range(len(self.table_rows) - 1):
+            self.assertIsInstance(self.table_rows[row], OrderedDict)
 
